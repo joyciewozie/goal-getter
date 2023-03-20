b371eaaa193d8bfbff7b940de474295b61d2637a
Joyce <121034558+joyciewozie@users.noreply.github.com>
2023-03-20 17:21:33 +0800

    create table for collaborators

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_091459) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_092055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "collaborators", force: :cascade do |t|
+    t.bigint "goal_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["goal_id"], name: "index_collaborators_on_goal_id"
+    t.index ["user_id"], name: "index_collaborators_on_user_id"
+  end
 
   create_table "goals", force: :cascade do |t|
     t.string "goal_type", null: false
@@ -37,5 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_091459) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "collaborators", "goals"
+  add_foreign_key "collaborators", "users"
   add_foreign_key "goals", "users"
 end
