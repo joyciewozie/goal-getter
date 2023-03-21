f17a0425b5067fb69902f399c7969c0d297b9b4a
Joyce <121034558+joyciewozie@users.noreply.github.com>
2023-03-21 16:52:46 +0800

    strong params for answer

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -2,4 +2,10 @@ class AnswerController < ApplicationController
   def new
     @answer = Answer.new
   end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:content)
+  end
 end
