e8ef1f20af3be73b09de41fb3264c894061443d0
Joyce <121034558+joyciewozie@users.noreply.github.com>
2023-03-22 15:18:56 +0800

    remove associations between goal and answers

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,7 +1,6 @@
 class Goal < ApplicationRecord
   belongs_to :user
   has_many :collaborators, dependent: :destroy
-  has_many :answers, dependent: :destroy
   has_many :highlights, dependent: :destroy
 
   validates :goal_type, :name, :description, :status, presence: true
