f7b47af654a0b3fcbc87093353f193a54e3d21b2
BingYou Leong <byleong1990@gmail.com>
2023-03-24 11:41:32 +0800

    missed out seed file from previous commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts 'Destroying all users'
+puts 'Clearing previous data'
 User.destroy_all
+Goal.destroy_all
+Insight.destroy_all
+TemplateQuestion.destroy_all
+Answer.destroy_all
+Highlight.destroy_all
+Collaborator.destroy_all
+Comment.destroy_all
+
 puts 'Creating user'
 user_one = User.create!(email: "jack@live.com", password: "123456", name: "Jack", gender: "Male", city: "Southampton", age: "25")
 user_two = User.create!(email: "rose@live.com", password: "123456", name: "Rose", gender: "Female", city: "Southampton", age: "24")
@@ -18,19 +26,16 @@ user_six = User.create!(email: "edward@live.com", password: "123456", name: "Edw
 
 
 puts 'Destroying all goals'
-Goal.destroy_all
 puts 'Creating goal'
 goal = Goal.create!(goal_type: "career", name: "Change jobs", description: "I'm bored at work and want to increase my income.", status: "in_progress", user_id: user_one.id)
 
 puts 'Destroying all insights'
-Insight.destroy_all
 puts 'Creating insight'
 insight_one = Insight.create!(name: "Job search", summary: "Narrowing down the job search be filtering roles and companies", goal_id: goal.id)
 insight_two = Insight.create!(name: "Trading bot", summary: "Earn passive income by using a DCA trading bot", goal_id: goal.id)
 insight_three = Insight.create!(name: "Rent out spare room", summary: "Rent out spare room for extra income", goal_id: goal.id)
 
 puts 'Destroying all template_questions'
-TemplateQuestion.destroy_all
 puts 'Creating template_question'
 template_question_one = TemplateQuestion.create!(title: "What type of job are you looking for?", goal_type: "career")
 template_question_two = TemplateQuestion.create!(title: "What specific roles are you looking for?", goal_type: "career")
@@ -42,7 +47,6 @@ template_question_seven = TemplateQuestion.create!(title: "How do you think you 
 template_question_eight = TemplateQuestion.create!(title: "What do you think are the potential hurdles in your way of problem-solving?", goal_type: "career")
 
 puts 'Destroying all answers'
-Answer.destroy_all
 puts 'Creating answer'
 answer_one = Answer.create!(content: "Software Developer", insight_id: insight_one.id, template_question: template_question_one)
 answer_two = Answer.create!(content: "Junior Java Developer", insight_id: insight_one.id, template_question: template_question_two)
@@ -54,12 +58,10 @@ answer_seven = Answer.create!(content: "Make the signal for opening the position
 answer_eight = Answer.create!(content: "If the signal is too strict, the bot may not open much trades.", insight_id: insight_two.id, template_question: template_question_eight)
 
 puts 'Destroying all highlights'
-Highlight.destroy_all
 puts 'Creating highlight'
 highlight = Highlight.create!(text: "Developer", answer_id: answer_one.id)
 
 puts 'Destroying all collaborators'
-Collaborator.destroy_all
 puts 'Creating collaborator'
 # collaborator_one = Collaborator.create!(goal: goal, user: user_one)
 collaborator_two = Collaborator.create!(goal: goal, user: user_two, accepted: true)
@@ -68,6 +70,5 @@ collaborator_four = Collaborator.create!(goal: goal, user: user_four)
 
 
 puts 'Destroying all comments'
-Comment.destroy_all
 puts 'Creating comment'
 comment = Comment.create!(content: "Great choice!", answer_id: answer_one.id, user_id: user_two.id)
