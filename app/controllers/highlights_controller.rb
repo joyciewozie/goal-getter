d41f130e35da46ae3f397edb79bc606da76b0d40
wei wei <weiyang.loh@gmail.com>
2023-03-24 14:53:56 +0800

    highlight functionality done

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -1,15 +1,19 @@
-class HighlightsController < ApplicationController
+ class HighlightsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
   def index
     render json: {}
   end
 
   def create
+    highlight = Highlight.new(highlights_params)
 
+    render :json => highlight
   end
 
   private
 
   def highlights_params
-    params.require(:highlight).permit(:text)
+    params.require(:highlight).permit(:text, :answer_id)
   end
 end
--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -2,6 +2,11 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="highlight"
 export default class extends Controller {
+  static values = {
+    answer: String,
+    text: String
+  }
+
   connect() {
     console.log("I'm here");
   }
@@ -17,7 +22,7 @@ export default class extends Controller {
       var scrollTop =
         window.pageYOffset !== undefined
           ? window.pageYOffset
-          : (
+           : (
               document.documentElement ||
               document.body.parentNode ||
               document.body
@@ -28,22 +33,33 @@ export default class extends Controller {
 
       document.body.insertAdjacentHTML(
         "beforeend",
-        '<div id="tool]tip" style="position:absolute; top: ' +
+        '<div data-controller="highlight" data-highlight-answer-value="' + this.answerValue + '" data-highlight-text-value="' + saveText +'" id="tooltip" style="position:absolute; top: ' +
           posY +
           "px; left: " +
           posX +
-          'px;"><div class="tooltiptext"><a href="#" onClick=\'window.open(menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600");\'><i class="fa fa-copy"></i> Save text</a></div></div>'
+          'px;"><div data-action="click->highlight#saveText" class="tooltiptext"><a><i class="fa fa-copy"></i> Save text</a></div></div>'
       );
     }
+  }
 
+  saveText() {
+    console.log(JSON.stringify({
+      "answer_id": this.answerValue,
+      "text": this.textValue
+    }))
     fetch("/highlights", {
       method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({"saveText": saveText})
-    })
-
-
-
-
+      headers: {
+        "Content-Type": "application/json"},
+      body: JSON.stringify({
+        "answer_id": this.answerValue,
+        "text": this.textValue
+      })
+    }).then((response)=>response.json())
+    .then((data)=>{
+      window.alert("bookmark saved")
+      console.log(document.getElementById("tooltip"))
+      document.getElementById("tooltip").remove()
+    });
   }
 }
